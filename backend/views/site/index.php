3fa52e9766dd18c37a74823f26b7fd79533931e7
Parantido De Rica <parantido@techfusion.it>
2015-08-21 16:53:29 +0200

    .

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -6,14 +6,10 @@ $this->title = Yii::$app->name;
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <h1>Welcome Home To:</h1>
+        <p class="lead">Open SBC</p>
     </div>
 
     <div class="body-content">
-
     </div>
 </div>
