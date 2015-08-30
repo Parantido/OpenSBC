5fefa13321bebc5c23e8dbccc2ca902733d55927
Parantido De Rica <parantido@techfusion.it>
2015-08-30 12:51:19 +0200

    .

--- backend/views/customers/index.php
+++ backend/views/customers/index.php
@@ -23,58 +23,62 @@ $this->params['breadcrumbs'][] = $this->title;
         <?php /* echo Html::a('Create Customers', ['create'], ['class' => 'btn btn-success'])*/  ?>
     </p>
 
-    <?php Pjax::begin(); echo GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-//            'id',
-            'firstname',
-            'lastname',
-            'address1',
-//            'address2',
-//            'city', 
-//            'country', 
-//            'province', 
-//            'state', 
-//            'zip', 
-//            'phone1', 
-//            'phone2', 
-//            'phone3', 
-//            'ptype1', 
-//            'ptype2', 
-//            'ptype3', 
-            'email:email',
-//            'skypeid', 
-//            'location', 
-//            'notes:ntext', 
-            'username',
-//            'password', 
-            'status',
-//            'domain_id', 
-
-            [
-                'class' => 'yii\grid\ActionColumn',
-                'buttons' => [
-                'update' => function ($url, $model) {
-                    return Html::a('<span class="glyphicon glyphicon-pencil"></span>', Yii::$app->urlManager->createUrl(['customers/view','id' => $model->id,'edit'=>'t']), [
-                        'title' => Yii::t('yii', 'Edit'),
-                    ]);}
+    <?php
+        Pjax::begin();
+        echo GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+    //            'id',
+                'firstname',
+                'lastname',
+                'address1',
+    //            'address2',
+    //            'city',
+    //            'country',
+    //            'province',
+    //            'state',
+    //            'zip',
+    //            'phone1',
+    //            'phone2',
+    //            'phone3',
+    //            'ptype1',
+    //            'ptype2',
+    //            'ptype3',
+                'email:email',
+    //            'skypeid',
+    //            'location',
+    //            'notes:ntext',
+                'username',
+    //            'password',
+                'status',
+    //            'domain_id',
+                [
+                    'class' => 'yii\grid\ActionColumn',
+                    'template' => '{update}',
+                    'buttons' => [
+                        'update' => function ($url, $model) {
+                            return Html::a('<span class="glyphicon glyphicon-pencil"></span>', Yii::$app->urlManager->createUrl(['customers/view','id' => $model->id,'edit'=>'t']), [
+                                'title' => Yii::t('yii', 'Edit'),
+                            ]);
+                        }
+                    ],
                 ],
             ],
-        ],
-        'responsive'=>true,
-        'hover'=>true,
-        'condensed'=>true,
-        'floatHeader'=>true,
+            'responsive'=>true,
+            'hover'=>true,
+            'condensed'=>true,
+            'floatHeader'=>true,
 
-        'panel' => [
-            'heading'=>'<h3 class="panel-title"><i class="glyphicon glyphicon-th-list"></i> '.Html::encode($this->title).' </h3>',
-            'type'=>'info',
-            'before'=>Html::a('<i class="glyphicon glyphicon-plus"></i> Add', ['create'], ['class' => 'btn btn-success']),                                                                                                                                                          'after'=>Html::a('<i class="glyphicon glyphicon-repeat"></i> Reset List', ['index'], ['class' => 'btn btn-info']),
-            'showFooter'=>false
-        ],
-    ]); Pjax::end(); ?>
+            'panel' => [
+                'heading'=>'<h3 class="panel-title"><i class="glyphicon glyphicon-th-list"></i> '.Html::encode($this->title).' </h3>',
+                'type'=>'info',
+                'before'=>Html::a('<i class="glyphicon glyphicon-plus"></i> Add', ['create'], ['class' => 'btn btn-success']),                                                                                                                                                          'after'=>Html::a('<i class="glyphicon glyphicon-repeat"></i> Reset List', ['index'], ['class' => 'btn btn-info']),
+                'showFooter'=>false
+            ],
+        ]);
+        Pjax::end();
+    ?>
 
 </div>
