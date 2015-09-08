f5c47cdd38bc7e7ed906c3bae57f25d29a48ea37
Parantido De Rica <parantido@techfusion.it>
2015-09-08 12:54:06 +0200

    .

--- backend/views/customers/_form.php
+++ backend/views/customers/_form.php
@@ -22,16 +22,18 @@ use kartik\widgets\ActiveForm;
         $domains_list = ArrayHelper::map(Domain::find()->all(), 'id', 'domain');
         $countries_list = ArrayHelper::map(Countries::find()->where(['countries_type' => '0'])->all(), 'countries_id', 'name');
 
+        $inputStyle = "padding-left: 10px; padding-right: 10px; padding-top: 2px; padding-bottom: 10px;";
+
         // Define Form Widget Columns
         $formColumns = [
-            'firstname'=>['attribute'=>'firstname', 'type'=> DetailView::INPUT_TEXT],
-            'lastname'=>['attribute'=>'lastname', 'type'=> DetailView::INPUT_TEXT],
-            'status'=>['attribute'=>'status', 'type'=> DetailView::INPUT_DROPDOWN_LIST],
-            'domain_id'=>['attribute'=>'domain_id', 'items' => $domains_list, 'type'=> DetailView::INPUT_DROPDOWN_LIST],
-            'address1'=>['attribute'=>'address1', 'type'=> DetailView::INPUT_TEXT],
-            'address2'=>['attribute'=>'address2', 'type'=> DetailView::INPUT_TEXT],
-            'state'=>['attribute'=>'state', 'items' => $countries_list, 'type'=> DetailView::INPUT_DROPDOWN_LIST, 'options'=>['id' => 'state-id']],
-            'city'=>['attribute'=>'city', 'type'=> DetailView::INPUT_DEPDROP, 'options' => ['id' => 'cities-id'], 'widgetOptions' => [
+            'firstname'=>['attribute'=>'firstname', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'lastname'=>['attribute'=>'lastname', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'status'=>['attribute'=>'status', 'type'=> DetailView::INPUT_DROPDOWN_LIST, 'inputContainer' => ['style' => $inputStyle]],
+            'domain_id'=>['attribute'=>'domain_id', 'items' => $domains_list, 'type'=> DetailView::INPUT_DROPDOWN_LIST, 'inputContainer' => ['style' => $inputStyle]],
+            'address1'=>['attribute'=>'address1', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'address2'=>['attribute'=>'address2', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'state'=>['attribute'=>'state', 'items' => $countries_list, 'type'=> DetailView::INPUT_DROPDOWN_LIST, 'options'=>['id' => 'state-id'], 'inputContainer' => ['style' => $inputStyle]],
+            'city'=>['attribute'=>'city', 'type'=> DetailView::INPUT_DEPDROP, 'inputContainer' => ['style' => $inputStyle], 'options' => ['id' => 'cities-id'], 'widgetOptions' => [
                 'pluginOptions' => [
                     'maxlength' => 50,
                     'depends' => ['state-id'],
@@ -39,21 +41,21 @@ use kartik\widgets\ActiveForm;
                     'url' => \yii\helpers\Url::to(['/customers/getcities'])
                 ],
             ]],
-            'country'=>['attribute'=>'country', 'type'=> DetailView::INPUT_TEXT],
-            'province'=>['attribute'=>'province', 'type'=> DetailView::INPUT_TEXT],
-            'zip'=>['attribute'=>'zip', 'type'=> DetailView::INPUT_TEXT],
-            'location'=>['attribute'=>'location', 'type'=> DetailView::INPUT_TEXT],
-            'phone1'=>['attribute'=>'phone1', 'type'=> DetailView::INPUT_TEXT],
-            'ptype1'=>['attribute'=>'ptype1', 'items' => $ptypes_list, 'type'=> DetailView::INPUT_DROPDOWN_LIST],
-            'phone2'=>['attribute'=>'phone2', 'type'=> DetailView::INPUT_TEXT],
-            'ptype2'=>['attribute'=>'ptype2', 'items' => $ptypes_list, 'type'=> DetailView::INPUT_DROPDOWN_LIST],
-            'phone3'=>['attribute'=>'phone3', 'type'=> DetailView::INPUT_TEXT],
-            'ptype3'=>['attribute'=>'ptype3', 'items' => $ptypes_list, 'type'=> DetailView::INPUT_DROPDOWN_LIST],
-            'email'=>['attribute'=>'email', 'type'=> DetailView::INPUT_TEXT],
-            'username'=>['attribute'=>'username', 'type'=> DetailView::INPUT_TEXT],
-            'password'=>['attribute'=>'password', 'type'=> DetailView::INPUT_PASSWORD],
-            'skypeid'=>['attribute'=>'skypeid', 'type'=> DetailView::INPUT_TEXT],
-            'notes'=>['attribute'=>'notes', 'type'=> DetailView::INPUT_TEXTAREA],
+            'country'=>['attribute'=>'country', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'province'=>['attribute'=>'province', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'zip'=>['attribute'=>'zip', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'location'=>['attribute'=>'location', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'phone1'=>['attribute'=>'phone1', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'ptype1'=>['attribute'=>'ptype1', 'items' => $ptypes_list, 'type'=> DetailView::INPUT_DROPDOWN_LIST, 'inputContainer' => ['style' => $inputStyle]],
+            'phone2'=>['attribute'=>'phone2', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'ptype2'=>['attribute'=>'ptype2', 'items' => $ptypes_list, 'type'=> DetailView::INPUT_DROPDOWN_LIST, 'inputContainer' => ['style' => $inputStyle]],
+            'phone3'=>['attribute'=>'phone3', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'ptype3'=>['attribute'=>'ptype3', 'items' => $ptypes_list, 'type'=> DetailView::INPUT_DROPDOWN_LIST, 'inputContainer' => ['style' => $inputStyle]],
+            'email'=>['attribute'=>'email', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'username'=>['attribute'=>'username', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'password'=>['attribute'=>'password', 'type'=> DetailView::INPUT_PASSWORD, 'inputContainer' => ['style' => $inputStyle]],
+            'skypeid'=>['attribute'=>'skypeid', 'type'=> DetailView::INPUT_TEXT, 'inputContainer' => ['style' => $inputStyle]],
+            'notes'=>['attribute'=>'notes', 'type'=> DetailView::INPUT_TEXTAREA, 'inputContainer' => ['style' => $inputStyle]],
         ];
 
         $form = ActiveForm::begin([
