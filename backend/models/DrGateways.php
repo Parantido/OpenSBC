172843bbe5edc3997977dc02cb8e38f7b9c038f6
Parantido De Rica <parantido@techfusion.it>
2015-09-11 16:58:23 +0200

    .

--- backend/models/DrGateways.php
+++ backend/models/DrGateways.php
@@ -40,11 +40,7 @@ class DrGateways extends \yii\db\ActiveRecord
     {
         return [
             ['cust_id', 'required'],
-            [['address'], 'required', function($attribute, $params) {
-                if(!preg_match('/^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3}):([0-9]{1,5})$/', $this->$attribute)) {
-                    $this->addError($attribute, 'Address should be filled with syntax IP:Port.');
-                }
-            }],
+            [['address'], 'match', 'pattern' => '/^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3}):([0-9]{1,5})$/', 'Address should be filled with syntax IP:Port.'],
             [['cust_id', 'type', 'strip', 'probe_mode', 'state'], 'integer'],
             [['gwid'], 'string', 'max' => 64],
             [['address', 'socket', 'description'], 'string', 'max' => 128],
