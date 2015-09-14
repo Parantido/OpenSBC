bfafc1a0954079f058cec7dcc9a40b59cfc63560
Parantido De Rica <parantido@techfusion.it>
2015-09-14 11:41:20 +0200

    .

--- backend/models/DrGateways.php
+++ backend/models/DrGateways.php
@@ -40,7 +40,8 @@ class DrGateways extends \yii\db\ActiveRecord
     {
         return [
             [['cust_id'], 'required'],
-            //[['address'], 'match', 'pattern' => '/^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3}):([0-9]{1,5})$/', 'Address should be filled with syntax IP:Port.'],
+            [['address'], 'validateGatewayAddress'],
+            ['address', 'match', 'pattern' => '//^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3}):([0-9]{1,5})$/i','message'=>Yii::t('app','Address should be filled with syntax IP:Port.')],
             [['cust_id', 'type', 'strip', 'probe_mode', 'state'], 'integer'],
             [['gwid'], 'string', 'max' => 64],
             [['address', 'socket', 'description'], 'string', 'max' => 128],
